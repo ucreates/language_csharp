dc7f363fc12e204da4702fac4b2d00705f1f019f
ucreates <ucreates.inc@gmail.com>
2026-05-17 17:26:37 +0900

    add object

--- Language/Object/ClassTest.cs
+++ Language/Object/ClassTest.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+
 namespace Language;
 
 public class ClassTest
@@ -6,35 +8,49 @@ public class ClassTest
     public void PublicClassObjectTest1()
     {
         var instance = new PublicClassObject();
-        Console.WriteLine($"{instance.ToString()}");
+        Console.WriteLine($"{instance}");
     }
 
     [Test]
     public void ProtectedClassObjectTest1()
     {
         var instance = new ProtectedClassObject();
-        Console.WriteLine($"{instance.ToString()}");
+        Console.WriteLine($"{instance}");
     }
 
     [Test]
     public void ProtectedInternalClassObjectTest1()
     {
         var instance = new ProtectedInternalClassObject();
-        Console.WriteLine($"{instance.ToString()}");
+        Console.WriteLine($"{instance}");
     }
 
     [Test]
     public void InternalClassObjectTest1()
     {
         var instance = new InternalClassObject();
-        Console.WriteLine($"{instance.ToString()}");
+        Console.WriteLine($"{instance}");
     }
 
     [Test]
     public void PrivateClassObjectTest1()
     {
         var instance = new PrivateClassObject();
-        Console.WriteLine($"{instance.ToString()}");
+        Console.WriteLine($"{instance}");
+    }
+
+    [Test]
+    public void BaseClassObjectTest1()
+    {
+        var instance = new BaseClassObject();
+        Console.WriteLine($"{instance}");
+    }
+
+    [Test]
+    public void InheritClassObjectTest1()
+    {
+        var instance = new InheritClassObject();
+        Console.WriteLine($"{instance}");
     }
 
     public class PublicClassObject
@@ -54,6 +70,18 @@ public class ClassTest
     }
 
     private class PrivateClassObject
+    {
+    }
+
+    public class BaseClassObject
+    {
+        public void Execute()
+        {
+            Console.WriteLine(MethodBase.GetCurrentMethod()?.Name);
+        }
+    }
+
+    public class InheritClassObject : BaseClassObject
     {
     }
 }
